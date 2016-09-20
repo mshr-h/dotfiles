1c8023a8244dc276d94ee88e607e0e3cafc7039e
mshr-h <mshr-h@users.noreply.github.com>
2016-09-20 11:25:17 +0900

    add git config for windows

--- windows.bat
+++ windows.bat
@@ -1,3 +1,11 @@
+REM  vim
 copy vim\\load.vimrc ..\\.vimrc
 copy vim\\load.gvimrc ..\\.gvimrc
 mklink /J ..\\.vim vim
+
+REM  git
+git config --global user.email "mshr-h"
+git config --global user.name "mshr-h@users.noreply.github.com"
+git config --global core.editor "vim"
+git config --global push.default "simple"
+
