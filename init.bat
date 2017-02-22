76b023259b4c4dd8c09b82eec5c3b9ec7e6d969d
mshr-h <mshr-h@users.noreply.github.com>
2017-02-22 16:55:59 +0900

    add anacrolix/torrent

--- init.bat
+++ init.bat
@@ -2,4 +2,5 @@
 
 go get github.com/mattn/files
 go get github.com/mattn/jvgrep
+go get github.com/anacrolix/torrent/cmd/torrent
 
--- init.sh
+++ init.sh
@@ -5,4 +5,5 @@ curl -sL zplug.sh/installer | zsh
 
 go get github.com/mattn/files
 go get github.com/mattn/jvgrep
+go get github.com/anacrolix/torrent/cmd/torrent
 
