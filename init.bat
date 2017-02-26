46819dbbc5bdc8e096a724c3f1956bffae9404a5
mshr-h <mshr-h@users.noreply.github.com>
2017-02-26 12:53:34 +0900

    add Code-Hex/pget

--- init.bat
+++ init.bat
@@ -2,5 +2,6 @@
 
 go get github.com/mattn/files
 go get github.com/mattn/jvgrep
+go get github.com/Code-Hex/pget
 go get github.com/anacrolix/torrent/cmd/torrent
 
--- init.sh
+++ init.sh
@@ -5,5 +5,6 @@ curl -sL zplug.sh/installer | zsh
 
 go get github.com/mattn/files
 go get github.com/mattn/jvgrep
+go get github.com/Code-Hex/pget
 go get github.com/anacrolix/torrent/cmd/torrent
 
