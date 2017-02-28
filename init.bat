6cb5c1392d54e4c2305fa691256243a8e667dda4
mshr-h <mshr-h@users.noreply.github.com>
2017-02-28 21:34:23 +0900

    remove torrent tools on windows

--- init.bat
+++ init.bat
@@ -3,7 +3,5 @@
 go get github.com/mattn/files
 go get github.com/mattn/jvgrep
 go get github.com/Code-Hex/pget
-go get github.com/anacrolix/torrent/cmd/torrent
-go get github.com/jackpal/Taipei-Torrent
 go get github.com/mshr-h/gomirror
 
