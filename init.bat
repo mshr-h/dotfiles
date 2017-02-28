77e9ca69712377aa5d5ba25610e17867b12a9804
mshr-h <mshr-h@users.noreply.github.com>
2017-02-28 21:28:42 +0900

    add mshr-h/gomirror

--- init.bat
+++ init.bat
@@ -5,4 +5,5 @@ go get github.com/mattn/jvgrep
 go get github.com/Code-Hex/pget
 go get github.com/anacrolix/torrent/cmd/torrent
 go get github.com/jackpal/Taipei-Torrent
+go get github.com/mshr-h/gomirror
 
