72fec54c87852bd67916755fb3a9fd1b9f1d0aa4
Masahiro H <mhg00g13@gmail.com>
2018-02-05 16:34:59 +0900

    remove unused tools

--- init.sh
+++ init.sh
@@ -3,8 +3,6 @@
 go get -u github.com/mattn/files
 go get -u github.com/mattn/jvgrep
 go get -u github.com/Code-Hex/pget
-go get -u github.com/anacrolix/torrent/cmd/torrent
-go get -u github.com/jackpal/Taipei-Torrent
 go get -u github.com/mshr-h/gomirror
 go get -u github.com/shurcooL/markdownfmt
 
