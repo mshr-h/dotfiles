1cc92dfee901a4c977bd2a3debaf9cc53cf22836
Masahiro H <mhg00g13@gmail.com>
2018-04-01 21:10:03 +0900

    fix pget url

--- init.sh
+++ init.sh
@@ -2,7 +2,6 @@
 
 go get -u github.com/mattn/files
 go get -u github.com/mattn/jvgrep
-go get -u github.com/Code-Hex/pget
+go get -u github.com/Code-Hex/pget/cmd/pget
 go get -u github.com/mshr-h/gomirror
 go get -u github.com/shurcooL/markdownfmt
-
