67524d146b1b7653dc002ebdeb0f9b533f180e97
Masahiro Hiramori <8973217+mshr-h@users.noreply.github.com>
2020-10-19 12:06:06 +0900

    remove git user and email setting

--- deploy.bat
+++ deploy.bat
@@ -5,8 +5,6 @@ copy vim\\load.gvimrc ..\\.gvimrc
 mklink /J ..\\.vim vim
 
 REM  git
-git config --global user.name "mshr-h"
-git config --global user.email "mshr-h@users.noreply.github.com"
 git config --global core.editor "vim"
 git config --global push.default "simple"
 
