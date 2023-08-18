c0f4ed891ad8926ed6f2982e3347594172ce7268
Masahiro Hiramori <mhg00g13@gmail.com>
2023-08-18 20:34:38 +0900

    set font size

--- wezterm.lua
+++ wezterm.lua
@@ -6,7 +6,8 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.initial_rows = 40
+config.initial_rows = 45
 config.initial_cols = 120
+config.font_size = 13.0
 
 return config
