bd45fdb2e300c42860464c1358420ca06191a8eb
Masahiro Hiramori <contact@mshr-h.com>
2024-08-24 16:38:26 +0900

    install llvm

--- install.sh
+++ install.sh
@@ -12,3 +12,6 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 && sudo apt update \
 && sudo apt install gh -y
+
+## LLVM
+sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
